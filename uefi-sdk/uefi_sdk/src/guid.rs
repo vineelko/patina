f347d29738123e0adf795a69b82f559c886b2dc7
Michael Kubacki <michael.kubacki@microsoft.com>
2024-12-02 18:57:26 -0500

    guid.rs: Add Exit Boot Services failed GUID doc
    Adds documentation for guid::EBS_FAILED.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- uefi-sdk/uefi_sdk/src/guid.rs
+++ uefi-sdk/uefi_sdk/src/guid.rs
@@ -11,6 +11,12 @@
 //!
 use r_efi::efi;
 
-// { 0x4f6c5507, 0x232f, 0x4787, { 0xb9, 0x5e, 0x72, 0xf8, 0x62, 0x49, 0xc, 0xb1 } }
+/// Exit Boot Services Failed GUID
+///
+/// The GUID for an event group signaled when ExitBootServices() fails. For example, the ExitBootServices()
+/// implementation may find that the memory map key provided does not match the current memory map key and return
+/// an error code. This event group will be signaled in that case just before returning to the caller.
+///
+/// (`4f6c5507-232f-4787-b95e-72f862490cb1`)
 pub const EBS_FAILED: efi::Guid =
     efi::Guid::from_fields(0x4f6c5507, 0x232f, 0x4787, 0xb9, 0x5e, &[0x72, 0xf8, 0x62, 0x49, 0x0c, 0xb1]);
