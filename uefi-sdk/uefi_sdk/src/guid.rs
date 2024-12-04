900ef53f8c770de401276627a194716a28bc7c37
John Schock <joschock@microsoft.com>
2024-12-04 12:48:04 -0800

    Add definitions for Zero and Memory Type Information GUIDs. (#9)
    ## Description
    
    Add definitions for Zero and Memory Type Information GUIDs.
    
    - [ ] Impacts functionality?
    - [ ] Impacts security?
    - [ ] Breaking change?
    - [ ] Includes tests?
    - [ ] Includes documentation?
    
    ## How This Was Tested
    
    Confirmed build and test passed.
    
    ## Integration Instructions
    
    N/A

--- uefi-sdk/uefi_sdk/src/guid.rs
+++ uefi-sdk/uefi_sdk/src/guid.rs
@@ -32,3 +32,23 @@ pub const EBS_FAILED: efi::Guid =
 /// (`23C9322F-2AF2-476A-BC4C-26BC88266C71`)
 pub const DXE_CORE: efi::Guid =
     efi::Guid::from_fields(0x23C9322F, 0x2AF2, 0x476A, 0xBC, 0x4C, &[0x26, 0xBC, 0x88, 0x26, 0x6C, 0x71]);
+
+/// Zero GUID
+///
+/// All-zero GUID, used as a marker or placeholder.
+///
+/// (`00000000-0000-0000-0000-000000000000`)
+pub const ZERO: efi::Guid = efi::Guid::from_fields(0, 0, 0, 0, 0, &[0, 0, 0, 0, 0, 0]);
+
+/// Memory Type Info GUID
+///
+/// The memory type information HOB and variable can be used to store information
+/// for each memory type in Variable or HOB.
+///
+/// The Memory Type Information GUID can also be optionally used as the Owner
+/// field of a Resource Descriptor HOB to provide the preferred memory range
+/// for the memory types described in the Memory Type Information GUID HOB.
+///
+/// (`4C19049F-4137-4DD3-9C10-8B97A83FFDFA`)
+pub const MEMORY_TYPE_INFORMATION: efi::Guid =
+    efi::Guid::from_fields(0x4C19049F, 0x4137, 0x4DD3, 0x9C, 0x10, &[0x8B, 0x97, 0xA8, 0x3F, 0xFD, 0xFA]);
