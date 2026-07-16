a477866969a5b8c0da7beb787e796273dd60f8c5
Kun Qin <kuqin@microsoft.com>
2026-07-16 15:59:09 -0700

    [supv] supv syscall index definitions
    Co-authored-by: Vineel Kovvuri[MSFT] <vineelko@microsoft.com>
    Signed-off-by: Kun Qin <kun.qin@microsoft.com>

--- sdk/patina/src/management_mode.rs
+++ sdk/patina/src/management_mode.rs
@@ -13,6 +13,7 @@ pub mod comm_buffer_hob;
 pub mod event;
 pub mod guid;
 pub mod protocol;
+pub mod supervisor;
 
 // Re-export commonly used items for easier access
 pub use comm_buffer_hob::MmCommBufferStatus;
