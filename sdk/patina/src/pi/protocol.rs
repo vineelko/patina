f211e6a902ca94110464dd26f2e56b9a2ada925c
Vineel Kovvuri[MSFT] <vineelko@microsoft.com>
2026-08-04 00:37:32 -0700

    Fixups

--- sdk/patina/src/pi/protocol.rs
+++ sdk/patina/src/pi/protocol.rs
@@ -14,9 +14,7 @@ pub mod communication;
 pub mod communication2;
 pub mod communication3;
 pub mod cpu_arch;
-#[cfg(any(test, feature = "alloc"))]
 pub mod firmware_volume;
-#[cfg(any(test, feature = "alloc"))]
 pub mod firmware_volume_block;
 pub mod metronome;
 pub mod runtime;
