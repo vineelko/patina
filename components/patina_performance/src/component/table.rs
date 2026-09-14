134e367d9c704e78a7cbe400c874f9697609a900
Michael Kubacki <michael.kubacki@microsoft.com>
2026-09-14 12:21:27 -0400

    patina_performance: Fix get_variable() call (#1775)
    ## Description
    
    The call should use a variable name, which is stored under
    "FirmwarePerformance". This updates the get_variable() call to use that
    name instead of an empty string.
    
    Note that this was an issue in the initial implementation in f632988 and
    not a recent regression.
    
    The variable data structure (`FirmwarePerformanceVariable`) should also
    be packed, so that is done here as well.
    
    - [x] Impacts functionality?
    - [ ] Impacts security?
    - [ ] Breaking change?
    - [x] Includes tests?
    - [ ] Includes documentation?
    
    ## How This Was Tested
    
    - `cargo make all` (includes unit test changes)
    
    Ran on patina-qemu with `patina_performance` enabled, log level set to
    `log::LevelFilter::Info`, and the following temporary message in
    `report_fbpt_event()`:
    
    ```rust
        if let Some(address) = find_previous_table_address(&runtime_services) {
            log::info!("Performance: Previous FBPT table found at address {address:#x}");
        }
    ```
    
    Verified the variable was found sucessfully:
    
    > INFO - Performance: Previous FBPT table found at address 0x7bef9000
    
    Note that the actual variable (even with the Patina component) is set in
    `FirmwarePerformanceDataTableDxe`
    [here](https://github.com/tianocore/edk2/blob/15ae5d75127415dece0c67354442926a076cf81a/MdeModulePkg/Universal/Acpi/FirmwarePerformanceDataTableDxe/FirmwarePerformanceDxe.c#L382-L388):
    
    ```c
      gRT->SetVariable (
             EFI_FIRMWARE_PERFORMANCE_VARIABLE_NAME,
             &gEfiFirmwarePerformanceGuid,
             EFI_VARIABLE_NON_VOLATILE | EFI_VARIABLE_BOOTSERVICE_ACCESS,
             sizeof (PerformanceVariable),
             &PerformanceVariable
             );
    ```
    
    ## Integration Instructions
    
    - N/A
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- components/patina_performance/src/component/table.rs
+++ components/patina_performance/src/component/table.rs
@@ -10,13 +10,13 @@
 use alloc::vec::Vec;
 use core::{mem, ptr};
 
-use patina::{BinaryGuid, Char16Str, uefi::runtime_services::RuntimeServices};
+use patina::{BinaryGuid, Char16Array, uefi::runtime_services::RuntimeServices};
 
 /// Return the address where the FBPT has been allocated during the previous boot.
 pub(crate) fn find_previous_table_address(runtime_services: &impl RuntimeServices) -> Option<usize> {
     runtime_services
         .get_variable::<FirmwarePerformanceVariable>(
-            Char16Str::EMPTY,
+            &FirmwarePerformanceVariable::ADDRESS_VARIABLE_NAME,
             &FirmwarePerformanceVariable::ADDRESS_VARIABLE_GUID,
             Some(mem::size_of::<FirmwarePerformanceVariable>()),
         )
@@ -25,13 +25,14 @@ pub(crate) fn find_previous_table_address(runtime_services: &impl RuntimeService
 }
 
 /// Struct used to get the value from the `FirmwarePerformanceVariable`
-#[repr(C)]
+#[repr(C, packed)]
 pub(crate) struct FirmwarePerformanceVariable {
     boot_performance_table_pointer: usize,
     _s3_performance_table_pointer: usize,
 }
 
 impl FirmwarePerformanceVariable {
+    const ADDRESS_VARIABLE_NAME: Char16Array<20> = Char16Array::from_str("FirmwarePerformance");
     const ADDRESS_VARIABLE_GUID: BinaryGuid = BinaryGuid::from_string("C095791A-3001-47B2-80C9-EAC7319F2FA4");
 }
 
@@ -61,7 +62,7 @@ mod tests {
             .expect_get_variable::<FirmwarePerformanceVariable>()
             .once()
             .withf(|name, namespace, size_hint| {
-                assert_eq!(Char16Str::EMPTY, name);
+                assert_eq!(FirmwarePerformanceVariable::ADDRESS_VARIABLE_NAME.as_char16_str(), name);
                 assert_eq!(&FirmwarePerformanceVariable::ADDRESS_VARIABLE_GUID, namespace);
                 assert_eq!(&Some(16), size_hint);
                 true
